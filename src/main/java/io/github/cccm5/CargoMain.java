784e738556b97b0c9bd620740ac8dedf472af02c
TylerS1066 <TylerS1066@users.noreply.github.com>
2025-03-16 14:00:19 -0500

    Extract config from main class

--- src/main/java/io/github/cccm5/CargoMain.java
+++ src/main/java/io/github/cccm5/CargoMain.java
@@ -10,6 +10,7 @@ import com.degitise.minevid.dtlTraders.utils.citizens.TraderTrait;
 import io.github.cccm5.async.LoadTask;
 import io.github.cccm5.async.ProcessingTask;
 import io.github.cccm5.async.UnloadTask;
+import io.github.cccm5.config.Config;
 import io.github.cccm5.util.CraftInventoryUtil;
 import io.github.cccm5.util.NPCUtil;
 import net.citizensnpcs.api.CitizensAPI;
@@ -17,6 +18,7 @@ import net.citizensnpcs.api.npc.NPC;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.craft.PlayerCraft;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -38,34 +40,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 public class CargoMain extends JavaPlugin implements Listener {
-    public static final String ERROR_TAG = ChatColor.RED + "Error: " + ChatColor.DARK_RED;
-    public static final String SUCCESS_TAG = ChatColor.DARK_AQUA + "Cargo: " + ChatColor.WHITE;
-    public static Logger logger;
     private static Economy economy;
     private static ArrayList<Player> playersInQue;
-    private static double unloadTax,loadTax;
     private static CargoMain instance;
-    private static int delay;//ticks
-    private static Material SIGN_POST = Material.getMaterial("SIGN_POST");
     private static Main dtlTradersPlugin;
-    private CraftManager craftManager;
-    private FileConfiguration config;
-    private boolean cardinalDistance;
-    private static boolean debug;
-    private double scanRange;
 
-    public void onEnable() {
-        logger = this.getLogger();
-        this.getServer().getPluginManager().registerEvents(this, this);
-        playersInQue = new ArrayList<Player>();
-        instance = this;
-        //************************
-        //*       Configs        *
-        //************************
-        config = getConfig();
+    private void loadConfig() {
+        FileConfiguration config = getConfig();
+
         config.addDefault("Scan range",100.0);
         config.addDefault("Transfer delay ticks",300);
         config.addDefault("Load tax percent", 0.01D);
@@ -73,27 +57,35 @@ public class CargoMain extends JavaPlugin implements Listener {
         config.addDefault("Cardinal distance",true);
         config.addDefault("Debug mode",false);
         config.options().copyDefaults(true);
-        this.saveConfig();
-        scanRange = config.getDouble("Scan range") >= 1.0 ? config.getDouble("Scan range") : 100.0;
-        delay = config.getInt("Transfer delay ticks");
-        loadTax = config.getDouble("Load tax percent")<=1.0 && config.getDouble("Load tax percent")>=0.0 ? config.getDouble("Load tax percent") : 0.01;
-        unloadTax = config.getDouble("Unload tax percent")<=1.0 && config.getDouble("Unload tax percent")>=0.0 ? config.getDouble("Unload tax percent") : 0.01;
-        cardinalDistance = config.getBoolean("Cardinal distance");
-        debug = config.getBoolean("Debug mode");
+        saveConfig();
+
+        Config.scanRange = config.getDouble("Scan range") >= 1.0 ? config.getDouble("Scan range") : 100.0;
+        Config.delay = config.getInt("Transfer delay ticks");
+        Config.loadTax = config.getDouble("Load tax percent")<=1.0 && config.getDouble("Load tax percent")>=0.0 ? config.getDouble("Load tax percent") : 0.01;
+        Config.unloadTax = config.getDouble("Unload tax percent")<=1.0 && config.getDouble("Unload tax percent")>=0.0 ? config.getDouble("Unload tax percent") : 0.01;
+        Config.cardinalDistance = config.getBoolean("Cardinal distance");
+        Config.debug = config.getBoolean("Debug mode");
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        playersInQue = new ArrayList<>();
+        instance = this;
+
+        loadConfig();
         //************************
         //*    Load Movecraft    *
         //************************
         if(getServer().getPluginManager().getPlugin("Movecraft") == null || !getServer().getPluginManager().getPlugin("Movecraft").isEnabled()) {
-            logger.log(Level.SEVERE, "Movecraft not found or not enabled");
+            getLogger().severe("Movecraft not found or not enabled");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        craftManager = CraftManager.getInstance();
         //************************
         //*    Load  Citizens    *
         //************************
         if(getServer().getPluginManager().getPlugin("Citizens") == null || !getServer().getPluginManager().getPlugin("Citizens").isEnabled()) {
-            logger.log(Level.SEVERE, "Citizens 2.0 not found or not enabled");
+            getLogger().severe("Citizens 2.0 not found or not enabled");
             getServer().getPluginManager().disablePlugin(this);	
             return;
         }
@@ -103,20 +95,24 @@ public class CargoMain extends JavaPlugin implements Listener {
         //*      Load Vault      *
         //************************
         if (getServer().getPluginManager().getPlugin("Vault") == null || !getServer().getPluginManager().getPlugin("Vault").isEnabled()) {
-            logger.log(Level.SEVERE, "Vault not found or not enabled");
+            getLogger().severe("Vault not found or not enabled");
             getServer().getPluginManager().disablePlugin(this);	
             return;
         }
+        //************************
+        //*    Load dtlTraders   *
+        //************************
         Plugin traders = getServer().getPluginManager().getPlugin("dtlTraders");
-        if (traders == null  || !(traders instanceof Main)){
+        if (!(traders instanceof Main)){
+            getLogger().severe("dtlTraders not found or not enabled");
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
         dtlTradersPlugin = (Main) traders;
         economy = getServer().getServicesManager().getRegistration(Economy.class).getProvider();
     }
 
     public void onDisable() {
-        logger = null;
         economy = null;
         instance = null;
     }
@@ -124,7 +120,7 @@ public class CargoMain extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) { // Plugin
         if (command.getName().equalsIgnoreCase("unload")) {
             if(!(sender instanceof Player)){
-                sender.sendMessage(ERROR_TAG + "You need to be a player to execute that command!");
+                sender.sendMessage(Config.ERROR_TAG + "You need to be a player to execute that command!");
                 return true;
             }
             unload((Player) sender);
@@ -133,7 +129,7 @@ public class CargoMain extends JavaPlugin implements Listener {
 
         if (command.getName().equalsIgnoreCase("load")) {
             if(!(sender instanceof Player)){
-                sender.sendMessage(ERROR_TAG + "You need to be a player to execute that command!");
+                sender.sendMessage(Config.ERROR_TAG + "You need to be a player to execute that command!");
                 return true;
             }
             load((Player) sender);
@@ -142,15 +138,15 @@ public class CargoMain extends JavaPlugin implements Listener {
 
         if (command.getName().equalsIgnoreCase("cargo")) {
             if(!sender.hasPermission("Cargo.cargo")){
-                sender.sendMessage(ERROR_TAG + "You don't have permission to do that!");
+                sender.sendMessage(Config.ERROR_TAG + "You don't have permission to do that!");
                 return true;
             }
             sender.sendMessage(ChatColor.WHITE + "--[ " + ChatColor.DARK_AQUA + "  Movecraft Cargo " + ChatColor.WHITE + " ]--");
-            sender.sendMessage(ChatColor.DARK_AQUA + "Scan Range: " + ChatColor.WHITE + scanRange + " Blocks");
-            sender.sendMessage(ChatColor.DARK_AQUA + "Transfer Delay: " + ChatColor.WHITE + delay + " ticks");
-            sender.sendMessage(ChatColor.DARK_AQUA + "Unload Tax: " + ChatColor.WHITE + String.format("%.2f",100*unloadTax) + "%");
-            sender.sendMessage(ChatColor.DARK_AQUA + "Load Tax: " + ChatColor.WHITE + String.format("%.2f",100*loadTax) + "%");
-            sender.sendMessage(ChatColor.DARK_AQUA + "Distance Type: " + ChatColor.WHITE + (cardinalDistance ? "Cardinal" : "Direct"));
+            sender.sendMessage(ChatColor.DARK_AQUA + "Scan Range: " + ChatColor.WHITE + Config.scanRange + " Blocks");
+            sender.sendMessage(ChatColor.DARK_AQUA + "Transfer Delay: " + ChatColor.WHITE + Config.delay + " ticks");
+            sender.sendMessage(ChatColor.DARK_AQUA + "Unload Tax: " + ChatColor.WHITE + String.format("%.2f",100*Config.unloadTax) + "%");
+            sender.sendMessage(ChatColor.DARK_AQUA + "Load Tax: " + ChatColor.WHITE + String.format("%.2f",100*Config.loadTax) + "%");
+            sender.sendMessage(ChatColor.DARK_AQUA + "Distance Type: " + ChatColor.WHITE + (Config.cardinalDistance ? "Cardinal" : "Direct"));
             return true;
         }
         return false;
@@ -187,10 +183,6 @@ public class CargoMain extends JavaPlugin implements Listener {
 
     }
 
-    public static boolean isDebug(){
-        return debug;
-    }
-
     public static Economy getEconomy(){
         return economy;
     }
@@ -199,35 +191,23 @@ public class CargoMain extends JavaPlugin implements Listener {
         return playersInQue;
     }
 
-    public static double getLoadTax(){
-        return loadTax;
-    }
-
-    public static double getUnloadTax(){
-        return unloadTax;
-    }
-
-    public static int getDelay(){
-        return delay;
-    }
-
     public static CargoMain getInstance(){
         return instance;
     }
 
     private void unload(Player player){
         if(!player.hasPermission("Cargo.unload")){
-            player.sendMessage(ERROR_TAG + "You don't have permission to do that!");
+            player.sendMessage(Config.ERROR_TAG + "You don't have permission to do that!");
             return;
         }
-        Craft playerCraft = craftManager.getCraftByPlayer(player);
+        PlayerCraft playerCraft = CraftManager.getInstance().getCraftByPlayer(player);
         if(playersInQue.contains(player)){
-            player.sendMessage(ERROR_TAG + "You're already moving cargo!");
+            player.sendMessage(Config.ERROR_TAG + "You're already moving cargo!");
             return;
         }
 
         if(playerCraft == null){
-            player.sendMessage(ERROR_TAG + "You need to be piloting a craft to do that!");
+            player.sendMessage(Config.ERROR_TAG + "You need to be piloting a craft to do that!");
             return;
         }
         //NPC cargoMerchant=null;
@@ -237,18 +217,18 @@ public class CargoMain extends JavaPlugin implements Listener {
         for(NPC npc :NPCUtil.getNPCsWithTrait(CargoTrait.class)){
             if(!npc.isSpawned())
                 continue;
-            distance = cardinalDistance ? Math.abs(loc.getX()-npc.getEntity().getLocation().getX()) + Math.abs(loc.getZ()-npc.getEntity().getLocation().getZ()) : Math.sqrt(Math.pow(loc.getX()-npc.getEntity().getLocation().getX(),2) + Math.pow(loc.getZ()-npc.getEntity().getLocation().getZ(),2));
-            if( distance <= scanRange){
+            distance = Config.cardinalDistance ? Math.abs(loc.getX()-npc.getEntity().getLocation().getX()) + Math.abs(loc.getZ()-npc.getEntity().getLocation().getZ()) : Math.sqrt(Math.pow(loc.getX()-npc.getEntity().getLocation().getX(),2) + Math.pow(loc.getZ()-npc.getEntity().getLocation().getZ(),2));
+            if( distance <= Config.scanRange){
                 nearbyMerchants.add(npc);
             }
         }
         if(nearbyMerchants.size()==0){
-            player.sendMessage(ERROR_TAG + "You need to be within " +  scanRange + " blocks of a merchant to use that command!");
+            player.sendMessage(Config.ERROR_TAG + "You need to be within " + Config.scanRange + " blocks of a merchant to use that command!");
             return;
         }
 
         if(player.getInventory().getItemInMainHand() == null || player.getInventory().getItemInMainHand().getType() == Material.AIR){
-            player.sendMessage(ERROR_TAG + "You need to be holding a cargo item to do that!");
+            player.sendMessage(Config.ERROR_TAG + "You need to be holding a cargo item to do that!");
             return;
         }
         String guiName;
@@ -273,7 +253,7 @@ public class CargoMain extends JavaPlugin implements Listener {
                     }
                 }
                     if (finalItem == null || finalItem.getTradePrice() == 0.0) {
-                        player.sendMessage(ERROR_TAG + "You need to be holding a cargo item to do that!");
+                        player.sendMessage(Config.ERROR_TAG + "You need to be holding a cargo item to do that!");
                         return;
                     }
 
@@ -285,29 +265,29 @@ public class CargoMain extends JavaPlugin implements Listener {
         List<Inventory> invs = CraftInventoryUtil.getInventories(playerCraft, finalItem.getMainItem(), Material.CHEST, Material.TRAPPED_CHEST, Material.BARREL);
         int size = invs.size();
         if(size <=0 ){
-            player.sendMessage(CargoMain.ERROR_TAG + "You have no " + itemName + " on this craft!");
+            player.sendMessage(Config.ERROR_TAG + "You have no " + itemName + " on this craft!");
             return;
         }
 
-        player.sendMessage(SUCCESS_TAG + "Started unloading cargo");
+        player.sendMessage(Config.SUCCESS_TAG + "Started unloading cargo");
         playersInQue.add(player);
-        new UnloadTask(craftManager.getCraftByPlayer(player),finalItem ).runTaskTimer(this,delay,delay);
+        new UnloadTask(playerCraft,finalItem ).runTaskTimer(this,Config.delay,Config.delay);
         new ProcessingTask(player, finalItem,size).runTaskTimer(this,0,20);
     }
 
     private void load(Player player){
         if(!player.hasPermission("Cargo.load")){
-            player.sendMessage(ERROR_TAG + "You don't have permission to do that!");
+            player.sendMessage(Config.ERROR_TAG + "You don't have permission to do that!");
             return;
         }
-        Craft playerCraft = craftManager.getCraftByPlayer(player);
+        PlayerCraft playerCraft = CraftManager.getInstance().getCraftByPlayer(player);
         if(playersInQue.contains(player)){
-            player.sendMessage(ERROR_TAG + "You're already moving cargo!");
+            player.sendMessage(Config.ERROR_TAG + "You're already moving cargo!");
             return;
         }
 
         if(playerCraft == null){
-            player.sendMessage(ERROR_TAG + "You need to be piloting a craft to do that!");
+            player.sendMessage(Config.ERROR_TAG + "You need to be piloting a craft to do that!");
             return;
         }
         //NPC cargoMerchant=null;
@@ -317,19 +297,19 @@ public class CargoMain extends JavaPlugin implements Listener {
         for(NPC npc :NPCUtil.getNPCsWithTrait(CargoTrait.class)){
             if(!npc.isSpawned())
                 continue;
-            distance = cardinalDistance ? Math.abs(loc.getX()-npc.getEntity().getLocation().getX()) + Math.abs(loc.getZ()-npc.getEntity().getLocation().getZ()) : Math.sqrt(Math.pow(loc.getX()-npc.getEntity().getLocation().getX(),2) + Math.pow(loc.getZ()-npc.getEntity().getLocation().getZ(),2));
-            if( distance <= scanRange){
+            distance = Config.cardinalDistance ? Math.abs(loc.getX()-npc.getEntity().getLocation().getX()) + Math.abs(loc.getZ()-npc.getEntity().getLocation().getZ()) : Math.sqrt(Math.pow(loc.getX()-npc.getEntity().getLocation().getX(),2) + Math.pow(loc.getZ()-npc.getEntity().getLocation().getZ(),2));
+            if( distance <= Config.scanRange){
                 nearbyMerchants.add(npc);
             }
         }
         if(nearbyMerchants.size()==0){
-            player.sendMessage(ERROR_TAG + "You need to be within " +  scanRange + " blocks of a merchant to use that command!");
+            player.sendMessage(Config.ERROR_TAG + "You need to be within " + Config.scanRange + " blocks of a merchant to use that command!");
             return;
         }
 
         if(player.getInventory().getItemInMainHand() == null || player.getInventory().getItemInMainHand().getType() == Material.AIR){
-            logger.info(player.getInventory().getItemInMainHand().getType().name());
-            player.sendMessage(ERROR_TAG + "You need to be holding a cargo item to do that!");
+            getLogger().info(player.getInventory().getItemInMainHand().getType().name());
+            player.sendMessage(Config.ERROR_TAG + "You need to be holding a cargo item to do that!");
             return;
         }
         String guiName;
@@ -363,27 +343,27 @@ public class CargoMain extends JavaPlugin implements Listener {
                 break;
         }
         if (finalItem == null || finalItem.getTradePrice() == 0.0) {
-            player.sendMessage(ERROR_TAG + "You need to be holding a cargo item to do that!");
+            player.sendMessage(Config.ERROR_TAG + "You need to be holding a cargo item to do that!");
             return;
         }
 
         final ItemMeta meta = finalItem.getMainItem().getItemMeta();
         String itemName = meta.getDisplayName() != null && meta.getDisplayName().length() > 0 ? meta.getDisplayName() : finalItem.getMainItem().getType().name().toLowerCase();
-        if(!economy.has(player,finalItem.getTradePrice()*(1+loadTax))){
-            player.sendMessage(ERROR_TAG + "You don't have enough money to buy any " + itemName + "!");
+        if(!economy.has(player,finalItem.getTradePrice()*(1+Config.loadTax))){
+            player.sendMessage(Config.ERROR_TAG + "You don't have enough money to buy any " + itemName + "!");
             return;
         }
 
         List<Inventory> invs = CraftInventoryUtil.getInventoriesWithSpace(playerCraft, finalItem.getMainItem(), Material.CHEST, Material.TRAPPED_CHEST, Material.BARREL);
         int size = invs.size();
         if(size <=0 ){
-            player.sendMessage(CargoMain.ERROR_TAG + "You don't have any space for " + itemName + " on this craft!");
+            player.sendMessage(Config.ERROR_TAG + "You don't have any space for " + itemName + " on this craft!");
             return;
         }
 
         playersInQue.add(player);
-        new LoadTask(craftManager.getCraftByPlayer(player),finalItem ).runTaskTimer(this,delay,delay);
+        new LoadTask(playerCraft,finalItem).runTaskTimer(this,Config.delay,Config.delay);
         new ProcessingTask(player, finalItem,size).runTaskTimer(this,0,20);
-        player.sendMessage(SUCCESS_TAG + "Started loading cargo");
+        player.sendMessage(Config.SUCCESS_TAG + "Started loading cargo");
     }
 }
--- src/main/java/io/github/cccm5/async/UnloadTask.java
+++ src/main/java/io/github/cccm5/async/UnloadTask.java
@@ -2,6 +2,7 @@ package io.github.cccm5.async;
 
 import com.degitise.minevid.dtlTraders.guis.items.TradableGUIItem;
 import io.github.cccm5.CargoMain;
+import io.github.cccm5.config.Config;
 import io.github.cccm5.util.CraftInventoryUtil;
 import net.countercraft.movecraft.craft.PlayerCraft;
 
@@ -25,16 +26,16 @@ public class UnloadTask extends CargoTask {
                 inv.setItem(i, null);
             }
         }
-        originalPilot.sendMessage(CargoMain.SUCCESS_TAG + "Unloaded " + count + " worth $"
+        originalPilot.sendMessage(Config.SUCCESS_TAG + "Unloaded " + count + " worth $"
                 + String.format("%.2f", count * item.getTradePrice()) + " took a tax of "
-                + String.format("%.2f", CargoMain.getUnloadTax() * count * item.getTradePrice()));
+                + String.format("%.2f", Config.unloadTax * count * item.getTradePrice()));
         CargoMain.getEconomy().depositPlayer(originalPilot,
-                count * item.getTradePrice() * (1 - CargoMain.getUnloadTax()));
+                count * item.getTradePrice() * (1 - Config.unloadTax));
 
         if (invs.size() <= 1) {
             this.cancel();
             CargoMain.getQue().remove(originalPilot);
-            originalPilot.sendMessage(CargoMain.SUCCESS_TAG + "All cargo unloaded");
+            originalPilot.sendMessage(Config.SUCCESS_TAG + "All cargo unloaded");
             return;
         }
         new ProcessingTask(originalPilot, item, invs.size()).runTaskTimer(CargoMain.getInstance(), 0, 20);
